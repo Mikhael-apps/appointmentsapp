d4e7a0675953052983fd25eb2c992742daf8ccec
mikhael_mike <mnosson@gmail.com>
2022-09-17 13:51:18 +0400

    build container
    Build container with design

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -33,25 +33,49 @@ class HomeScreen extends StatelessWidget {
       ),
       body: Column(
         children: <Widget>[
-          SearchHomeScreen(),
+          const SearchHomeScreen(),
           Container(
-            decoration: BoxDecoration(
-              color: Colors.blue.shade300,
-              borderRadius: BorderRadius.circular(25),
-            ),
-            width: 365,
-            height: 210,
-            child: Column(
-              children: const <Widget>[
-                Text('Medikal Ckecks', style: TextStyle(
-                    color: Colors.white, 
-                    fontSize: 25,
-                    fontWeight: FontWeight.bold,
+              decoration: BoxDecoration(
+                // image: DecorationImage(image: AssetImage('images/bg.jpg'), fit: BoxFit.cover),
+                color: Colors.blue.shade300,
+                borderRadius: BorderRadius.circular(25),
+              ),
+              width: MediaQuery.of(context).size.width * 0.91,
+              height: MediaQuery.of(context).size.height * 0.25,
+              child: Stack(
+                children: <Widget>[
+                  Positioned(
+                    left: 25,
+                    top: 30,
+                    child: Text(
+                      'Medical Checks!',
+                      style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 27,
+                          fontWeight: FontWeight.bold),
                     ),
+                  ),
+                  Positioned(
+                    left: 25,
+                    top: 75,
+                    child: Text(
+                      'Check your health condition regulary \n to minimize the incidence to disease in \n the future',
+                      style: TextStyle(color: Colors.white),
                     ),
-              ],
-            ),
-          ),
+                  ),
+                  Positioned(
+                    left: 25,
+                    top: 135,
+                    child: Container(
+                    child: Center(child: Text('Check Now')),
+                    width: 100,
+                    height: 35,
+                    decoration: BoxDecoration( color: Colors.white, borderRadius: BorderRadius.circular(35)),
+                    
+                  ),
+                  )
+                ],
+              )),
         ],
       ),
     );
